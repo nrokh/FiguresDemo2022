046f95d7d22951b742ceb86902fb53dc25f74ca7
nrokh <n.rokhmanova@gmail.com>
2022-09-16 11:14:44 +0200

    add matlab

---
+++ MATLAB/sampleData.m
@@ -0,0 +1,17 @@
+%%%%%%%%%%%%%%%%%
+%  Figures Demo %
+%   nrokh 2022  %     
+%%%%%%%%%%%%%%%%%
+
+% 1. Create fake data to plot
+t = linspace(0,15,100); 
+y1 = 5*sin(t+2);
+y2 = 4*sin(0.85*t+1.5);
+
+% 2. Plot data
+figure; hold on;
+set(gca, 'FontSize', 14)
+plot(t,y1);
+plot(t,y2);
+xlabel('Time (s)')
+ylabel('Magic (V)')
